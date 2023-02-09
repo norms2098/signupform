4f37b31b66c2323c01a893aa87bb1d7ed9c369f9
norms2098 <nrmsdgdgn@gmail.com>
2023-02-09 14:57:56 +0800

    added password matching condition

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-/*const inputField = document.getElementsByTagName("input")
+const password=document.getElementById('password');
+const passwordConfirm=document.getElementById('confirm_pass');
+const submitBtn = document.querySelector('.submit-btn')
 
-for (inputs in inputField){
-    if(inputField.innerHTML==="")
-}*/
+
+function errorMessage(){
+    const errorDisplay=document.querySelector('.error-msg');
+    errorDisplay.innerHTML="*Passwords don't match.";
+    password.style.borderColor="red";
+    passwordConfirm.style.borderColor="red";
+    errorDisplay.style.fontWeight="bold";
+    errorDisplay.style.marginTop="5px";
+}
+
+submitBtn.addEventListener('click', (e) => {
+   
+    if(password.value !== passwordConfirm.value){
+        e.preventDefault();
+        errorMessage();
+    }
+
+})
